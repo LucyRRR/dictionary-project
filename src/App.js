1d43d69aeb2d1759e3682deb5fb3285cd207d0e8
LucyRRR <lucka.roth@gmail.com>
2021-06-24 16:02:54 +0200

    inserted search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       Hello
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>
+        Coded by Lucia Rothova
+        </small>
+      </footer>
+      </div>
     </div>
   );
 }
